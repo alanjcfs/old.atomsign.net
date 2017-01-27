03f889648f5bbf36497b196f47e957982af941e3
Alan Fung-Schwarz <alan@atomsign.net>
2017-01-26 19:36:32 -0500

    Add Procfile, fix require, add scripts

--- elm/src/index.js
+++ elm/src/index.js
@@ -4,7 +4,7 @@ require('ace-css/css/ace.css');
 // require('font-awesome/css/font-awesome.css'); // install font-awesome@4
 
 // Ensure index.html gets copied to dist
-require('./index.html');
+require('../index.html');
 
 var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
