839808caf0014a2e85ae331e494474927b2f3e1e
Alan Fung-Schwarz <alan@atomsign.net>
2017-03-27 20:50:20 -0400

    Making Elm work!

--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,48 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (Html, div, text, program, span, ul, li)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 
+import Markdown exposing (toHtml)
 
+home = toHtml [ class "content" ]"""
+# Home
+
+Welcome to my home page.
+
+There's nothing here yet.
+"""
+
+school = toHtml [ class "content" ] """
+# School
+
+I went to UC Berkeley.
+"""
+
+work = toHtml [ class "content" ] """
+# Work
+
+I work at Citadel
+"""
 
 -- MODEL
 
 type alias Model =
-    String
+    { page : Html Msg
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( "Hello", Cmd.none )
+    ( Model (toHtml [ class "content" ] """Hello, this was written in Elm."""), Cmd.none )
 
 -- MESSAGES
 
 type Msg
-    = NoOp
+    = Home
+    | School
+    | Work
 
 
 
@@ -25,15 +50,30 @@ type Msg
 view : Model -> Html Msg
 view model =
     div []
-        [ text model ]
+        [ div [ class "nav" ]
+            [ ul []
+                [ li [ onClick Home ] [ text "Home" ]
+                , li [ onClick School ] [ text "School" ]
+                , li [ onClick Work ] [ text "Work" ]
+                ]
+            , div [] [ model.page ]
+            ]
+        ]
+
 
 -- UPDATE
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        Home ->
+            ( { page = home }, Cmd.none )
+        School ->
+            ( { page = school }, Cmd.none )
+        Work ->
+            ( { page = work }, Cmd.none )
+
+
 
 
 -- SUBSCRIPTIONS
