90d37206017b050796bc0f7f26bc4ef0e56c691e
Alan Fung-Schwarz <alan@atomsign.net>
2017-06-13 18:45:23 -0400

    Bump webpack to 2

--- webpack.config.js
+++ webpack.config.js
@@ -13,34 +13,35 @@ module.exports = {
   },
 
   module: {
-    loaders: [
-      {
-        test: /\.(css|scss)$/,
-        loaders: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
+    rules: [
       {
         test: /\.html$/,
         exclude: /node_modules/,
-        loader: 'file?name=[name].[ext]',
-      },
-      {
+        use: {
+          loader: 'file-loader?name=[name].[ext]',
+        }
+      }, {
+        test: /\.(css|scss)$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }, {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader: 'elm-webpack?verbose=true&warn=true',
-      },
-      {
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {}
+        }
+      }, {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      {
+        use: {
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        }
+      }, {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-      },
+        use: {
+          loader: 'file-loader',
+        }
+      }
     ],
-
     noParse: /\.elm$/,
   },
 
