cf71fced5a98a880579668e9c88fd66c5931992f
Alan Fung-Schwarz <alan@atomsign.net>
2017-06-21 15:57:28 -0400

    Remove indentation as Markdown interprets undesirably

--- src/School.elm
+++ src/School.elm
@@ -9,16 +9,14 @@ import Msg exposing (..)
 school : Model
 school = Model (toHtml [ class "content" ] text)
 
-text =
-    """
-    # School
+text = """
+# School
 
-    I received a BA in Molecular and Cell Biology from UC Berkeley and an MS in
-                       Chemistry from CSU East Bay.  After a time as a lab
-                       assistant, I decided to change career to programming.
-                       Although I had taken some programming classes
+I received a BA in Molecular and Cell Biology from UC Berkeley and an MS in
+Chemistry from CSU East Bay.  After a time as a lab assistant, I decided to
+change career to programming.  Although I had taken some computer science courses
+in different schools, with background in data structure, algorithms, and
+computer architecture, I don't have a formal degree in CS.
 
-
-
-    """
+"""
 
