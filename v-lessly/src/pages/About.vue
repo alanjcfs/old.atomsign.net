141cdc0c79176599d56ee8c8a519e33529ec30dd
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2020-10-07 21:59:08 -0700

    Committed

--- v-lessly/src/pages/About.vue
+++ v-lessly/src/pages/About.vue
@@ -1,7 +1,15 @@
 <template>
   <Layout>
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>
+      I am a long-term investor (or I strive to be) by investing in the total
+      stock market fund. You can find out how I am saving half of my income
+      every year and investing in a combination of total US stock market and
+      total international stock market. Although I think that the US stock
+      market will outperform the international stock market, investing
+      internationally provides diversification and is a noncorrelated asset. It
+      is also a potential inflation hedge as the US dollar fluctuates.
+    </p>
   </Layout>
 </template>
 
